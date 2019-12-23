f8bda3543744b62816580f82bd36c79725697900
oozonomomoko <oozonomomoko@github.com>
2019-12-23 16:35:18 +0800

    date

--- v1/js/datainit.js
+++ v1/js/datainit.js
@@ -99,7 +99,7 @@ $(document).ready(function () {
     var href = window.location.href;
     var id = href.split("?")[1].split("&")[0].split("=")[1].split("#")[0];
         
-    var domain = id<'52056'?'/blog/':'/blogbak/';
+    var domain = id<52056?'/blog/':'/blogbak/';
     function getDetailPic(pic, year){
         return pic.replace(/http.*ikuta\.club\/nogizaka\//,domain).replace(/http.*img\.nogizaka46\.com\/www\/smph\/member\/img/, '../image/head').replace(/\/\d{8}\//,'/'+year+'/');
     }
